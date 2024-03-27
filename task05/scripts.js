1b11c926b9d1e1e3e9f54acb43b6ebb59202946b
Kudl1k <stepanek.kudlacek@gmail.com>
2024-03-27 11:40:19 +0100

    done

--- task05/scripts.js
+++ task05/scripts.js
@@ -1,32 +1,58 @@
-const fetch_data = async () => {
-    const name = 'KUD0132';
-    const URL = 'https://homel.vsb.cz/~mor03/TAMZ/TAMZ22.php';
+const urlInput = document.querySelector("#url");
+const loginInput = document.querySelector("#login");
+const receivedOutput = document.querySelector("#received");
+const decocedOutput = document.querySelector("#decoded");
+const messageOutput = document.querySelector("#message");
+
+const getCodeButton = document.querySelector("#get-code");
+const sendCodeButton = document.querySelector("#send-code");
+
+
+let data = {}
+
+data['url'] = urlInput.value;
+data['login'] = loginInput.value;
+
+console.log(data['login'])
+
+urlInput.addEventListener('ionInput', function (e) {
+    data['url'] = e['detail']['value'];
+});
+loginInput.addEventListener('ionInput', function (e) {
+    data['login'] = e['detail']['value'];
+});
+getCodeButton.addEventListener('click', async (e) => {
+    const name = data['login'];
+    const URL = data['url'];
+    console.log(URL);
     const params = new URLSearchParams();
     params.append('user', name);
     params.append('timestamp', Date.now().toString());
-
     try {
-        const response = await fetch(`${URL}?${params.toString()}`);
-        const token = await response.text();
+      const response = await fetch(`${URL}?${params.toString()}`);
+      const token = await response.text();
+      data['token'] = token;
+      receivedOutput.value = token;
+      decocedOutput.value = atob(token);
 
-        const response2 = await fetch(URL, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-        });
-        
-        const data = await response2.text();
-
-        document.getElementById('data-display').textContent = data;
+      console.log(data['token'])
     } catch (error) {
         console.error('Error fetching data:', error);
     }
-};
+});
+sendCodeButton.addEventListener('click', async (e) => {
+    const URL = data['url'];
+    const token = data['token'];
+    try {
+      const response2 = await fetch(URL, {
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      });
 
-
-const fetch_button = document.getElementById('fetch-data');
-
-fetch_button.addEventListener('click', function() {
-    fetch_data()
-})
-
+      const message = await response2.text();
+      messageOutput.innerHTML = message;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
