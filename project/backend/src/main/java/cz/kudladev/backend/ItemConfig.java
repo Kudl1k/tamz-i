2078935ef761acd885e34bf19b72826696f3b35e
Kudl1k <stepanek.kudlacek@gmail.com>
2024-05-09 10:53:12 +0200

    fix the backend issues

--- project/backend/src/main/java/cz/kudladev/backend/ItemConfig.java
+++ project/backend/src/main/java/cz/kudladev/backend/ItemConfig.java
@@ -1,13 +1,15 @@
 package cz.kudladev.backend;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class ItemConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Add the origin of your frontend application
+                .allowedOrigins("http://127.0.0.1:5500/") // Add the origin of your frontend application
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
